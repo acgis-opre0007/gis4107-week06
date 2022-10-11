00b1f284189d619c447348e8e1bb2679bc530bfe
Chris Doughty <doug0181@algonquincollege.com>
2022-10-11 14:22:56 -0400

    initial part 2

--- doc_scanner.py
+++ doc_scanner.py
@@ -3,3 +3,9 @@ def has_x_code(in_text):
     in_text -- initial string"""
 
     return('Tx6op3' in in_text)
+
+def get_x_code_position(in_text):
+    if ('Tx6op3' in in_text) == True:
+        return(in_text.find('Tx6op3')+1)
+    else:
+        return(-1)
--- test_doc_scanner.py
+++ test_doc_scanner.py
@@ -3,5 +3,12 @@ import doc_scanner
 def test_has_x_code(in_text):
     return(doc_scanner.has_x_code(in_text))
 
+def test_get_x_code_position(in_text):
+    return(doc_scanner.get_x_code_position(in_text))
+
 print(f'\nExpected: False\nActual: {test_has_x_code("Hello")}')
 print(f'\nExpected: True\nActual: {test_has_x_code("Tx6op3")}')
+
+print(f'\nExpected: -1\nActual: {test_get_x_code_position("Hello")}')
+print(f'\nExpected: 1\nActual: {test_get_x_code_position("Tx6op3")}')
+print(f'\nExpected: 8\nActual: {test_get_x_code_position("Extend Tx6op3")}')
