7a98770c36d12d8a9dd7662bec1f843a26bac170
Chris Doughty <doug0181@algonquincollege.com>
2022-11-01 00:28:04 -0400

    troubleshooting dms converter and test

--- dms_converter.py
+++ dms_converter.py
@@ -9,22 +9,23 @@ def dms2dd(initial_record_dms):
     #initial_record_dms = str(initial_record_dms.splitlines()[0])
     long = 0
     lat = 0
+    parameterLength = len(initial_record_dms)
 
-    if len(initial_record_dms) == 22:   #checks for valid length, as in DDD MM SS W|E DD MM SS N|S
+    if len(initial_record_dms) == 23:   #checks for valid length, as in DDD MM SS W|E DD MM SS N|S
         working_elements = initial_record_dms.split(' ') #DMS string is split into list
 
         for i in range(0,len(working_elements)): #iterates through full list
             if i == 3: #east-west direction item
-                if working_elements[i] != 'E' or 'e' or 'W' or 's': #checks if direction is not valid
-                    print("ERR 1: bad E|W")
+                if working_elements[i] != 'E' and working_elements[i] != 'e' and working_elements[i] != 'W' and working_elements[i] != 'w': #checks if direction is not valid
+                    print("\nERR 1: bad E|W")
                     return('None, None')
             elif i == 7: #north-south direction item
-                if working_elements[i] != 'N' or 'n' or 'S' or 's':
-                    print("ERR 2: bad N|S")
+                if working_elements[i] != 'N' and working_elements[i] != 'n' and working_elements[i] != 'S' and working_elements[i] != 's':
+                    print("\nERR 2: bad N|S")
                     return('None, None')
             else: #all other non-directional items, which should be numbers
-                if working_elements == False:
-                    print("ERR 3: bad DMS values")
+                if working_elements[i].isdigit() == False:
+                    print("\nERR 3: bad DMS values")
                     return('None, None')
         
         for i in range(0,3): #iterates through longitude deg/min/sec and converts to decimal
@@ -34,18 +35,18 @@ def dms2dd(initial_record_dms):
             lat = lat + int(working_elements[4+i])/(60**i)
         
         if long > 180 or long < 0 or lat > 90 or lat < 0: #checks resultant values against domain
-            print("ERR 4: DD out of domain")
+            print("\nERR 4: out of domain")
             return('None, None') #return value as failure if outside domain
         
-        if working_elements[4] == 'S' or 's': #interpret north-south direction as sign
+        if working_elements[7] == 'S' or working_elements[7] == 's': #interpret north-south direction as sign
             lat = lat*(-1) #south as negative
         
-        if working_elements[3] == 'W' or 'w': #interpret east-west direction as sign
+        if working_elements[3] == 'W' or working_elements[7] == 'w': #interpret east-west direction as sign
             long = long*(-1) #west as negative
         
-        record_dd = str(long) + ", " + str(lat)
+        record_dd = f'{long:.3f}' + ", " + f'{lat:.3f}'
         
         return(record_dd)
     else:
-        print("ERR 5: bad length")
+        print("\nERR 5: bad length")
         return('None, None') #return value as failure if not of valid length
--- test_dms_converter.py
+++ test_dms_converter.py
@@ -2,8 +2,10 @@ import dms_converter
 
 def test_dms2dd(initial_record_dms):
     return(dms_converter.dms2dd(initial_record_dms))
-
-print(f'\nInitial Record: Hello World (invalid input)\nExpected: None, None\nActual: {test_dms2dd("Hello World")}')
-print(f'\nInitial Record: 075 45 03 W 45 23 05 N\\n\nExpected: -75.751, 45.385\nActual: {test_dms2dd("075 45 03 W 45 23 05 N")}')
-#print(f'\nInitial Record: Hello World (invalid input)\nExpected: None, None\nActual: {test_dms2dd("Hello World")}')
-#print(f'\nInitial Record: Hello World (invalid input)\nExpected: None, None\nActual: {test_dms2dd("Hello World")}')
+print('\n')
+print(f'Initial Record: 075 45 03 W 045 23 05 N\nExpected: -75.751, 45.385\nActual: {test_dms2dd("075 45 03 W 045 23 05 N")}\n')
+print(f'Initial Record: 075 45 03 L 045 23 05 N (bad east-west indicator)\nExpected: None, None\nActual: {test_dms2dd("075 45 03 L 045 23 05 N")}\n')
+print(f'Initial Record: 075 45 03 W 045 23 05 U (bad north-south indicator)\\n\nExpected: None, none\nActual: {test_dms2dd("075 45 03 W 045 23 05 U")}\n')
+print(f'Initial Record: yes 45 03 W deg 23 05 N (bad DMS values)\\n\nExpected: -75.751, 45.385\nActual: {test_dms2dd("yes 45 03 W deg 23 05 N")}\n')
+print(f'Initial Record: 200 10 10 E 100 10 10 N (outside range domain)\nExpected: None, None\nActual: {test_dms2dd("200 10 10 E 100 10 10 N")}\n')
+print(f'Initial Record: Hi (too short)\nExpected: None, None\nActual: {test_dms2dd("Hi")}\n')
